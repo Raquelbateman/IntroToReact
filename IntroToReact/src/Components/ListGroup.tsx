6086e6c61874b11920e476a8034e989dbe5d58ac
Raquelbateman <aliciaraquel1017@gmail.com>
2024-05-06 19:20:52 -0700

    update

--- IntroToReact/src/Components/ListGroup.tsx
+++ IntroToReact/src/Components/ListGroup.tsx
@@ -2,7 +2,13 @@
 
 const ListGroup = () => {
   return (
-    <div>ListGroup</div>
+    <ul className="list-group">
+    <li className="list-group-item">An item</li>
+    <li className="list-group-item">A second item</li>
+    <li className="list-group-item">A third item</li>
+    <li className="list-group-item">A fourth item</li>
+    <li className="list-group-item">And a fifth one</li>
+  </ul>
   )
 }
 
