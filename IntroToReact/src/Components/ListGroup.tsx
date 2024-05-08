bcd9648156dabc68ee113741e2c35cf4c44897c8
Raquelbateman <aliciaraquel1017@gmail.com>
2024-05-07 19:35:32 -0700

    update

--- IntroToReact/src/Components/ListGroup.tsx
+++ IntroToReact/src/Components/ListGroup.tsx
@@ -1,29 +1,43 @@
+import { useState } from "react";
+
+
 const ListGroup = () => {
   let items = ["New York", "Tokyo", "London", "Paris"];
+  let selectedIndex = 0;
 
-    // items = []
-    // if(items.length == 0)
-    //     {
-    //         return 
-    //         <>
-    //         <h1>List</h1>
-    //         <p>No Items found</p>
-    //         </>
-    //     };
+  const arr = useState(0);
 
+  console.log(arr);
+  // console.log(useState(-1));
+  //Event handler which is a function to handle an event
+  // e stands for event
+  // const handleClick = (e: MouseEvent) => {
+  //   console.log(e.target.innerText);
+  // };
 
+  // items = [];
 
+  const getMessage = () => {
+    return items.length == 0 ? <p>No Items Found</p> : null;
+  };
 
   return (
     //JSX we must have one parent element. Example, div, fragment or <></>
 
     <>
-  {items.length == 0 ? <p>No Items Found</p>: null}
-   
       <h1>List</h1>
+      {getMessage()}
       <ul className="list-group">
         {items.map((item, index) => (
-          <li className="list-group-item" key={index}>
+          <li
+            className={
+              selectedIndex == index
+              ? 'list-group-item active'
+               : 'list-group-item'} 
+               key={index}
+               onClick={() => console.log('${item}')}
+
+          >
             {item}
           </li>
         ))}
