e631f13ffa3d19477c2b16c1055c866f2512775c
Raquelbateman <aliciaraquel1017@gmail.com>
2024-05-14 18:31:56 -0700

    update

--- IntroToReact/src/Components/ListGroup/ListGroup.tsx
+++ IntroToReact/src/Components/ListGroup/ListGroup.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import './ListGroup.css'
+import styles from './ListGroup.module.css'
 
 // to make this reusable we will be using properties
 // TypeScript has a feature called interface
@@ -83,7 +83,7 @@ const ListGroup = ({items,heading,onSelectedItem}:ListProps) => {
       {getMessage()}
       {/* this will function the same way as above in line 7 to 16 */}
       {/* {items.length == 0 ? <p>No Items Found</p> : null} */}
-      <ul className="list-group">
+      <ul className={'list-group'}>
         {/* This was step 2 <li className="list-group-item">An item</li>
         <li className="list-group-item">A second item</li>
         <li className="list-group-item">A third item</li>
@@ -104,8 +104,8 @@ const ListGroup = ({items,heading,onSelectedItem}:ListProps) => {
             // onClick={handleClick}
             className={
               selectedIndex == index
-                ? "list-group-item active"
-                : "list-group-item"
+                ? styles['list-group-item']
+                :styles [`list-group-item`]
             }
             key={index}
             // onClick={() => console.log(`${item}`)}
